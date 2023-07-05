c87a7f58d2404dd4dc3a6901083235aed9e9359b
Anderson <eusouanderson@outlook.com>
2023-07-05 09:51:48 -0300

    "update"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-10
+
+
+//bool = true or false
+//char = single character
+//byte = 0 to 255
+//int  = -2147483648 to 2147483647
+//long = -9223372036854775808 to 9223372036854775807
+//float = -3.402823466e+38 to 3.402823466e+38
+//double = -1.7976931348623157e+308 to 1.7976931348623157e+308
+//decimal = -1.7976931348623157e+308 to 1.7976931348623157e+308
+//string = " Hello World "
