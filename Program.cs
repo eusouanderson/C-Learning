33956b2064b2898dd15260bbe765e01bb78499c5
Anderson <eusouanderson@outlook.com>
2023-07-05 10:44:56 -0300

    "update"

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite seu nome: ");
 
-System.
+string name = Console.ReadLine();
 
+Console.WriteLine("Digite seu sobrenome: ");
 
+string lastname = Console.ReadLine();
+
+Console.Write("Digite o ano de nascimento: ");
+
+int year = int.Parse(Console.ReadLine());
+
+int age = 2023 - year;
+
+Console.WriteLine($"Você nasceu no ano: {year} e seu nome é: {name} {lastname} e sua idade é: {age}");
