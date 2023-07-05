dcfec8a5fe618416654587f17edefb93aec1a4a1
Anderson <eusouanderson@outlook.com>
2023-07-05 17:48:59 -0300

    "update"

--- Program.cs
+++ Program.cs
@@ -1,25 +1,9 @@
-﻿//ARRAYS 
-
-string[] names = {"Anderson", "Silva", "João"};
-
-int[] numbers = {1, 2, 3};
-
-float[] floats = {1.1f, 2.2f, 3.3f};
-
-double[] doubles = {1.1, 2.2, 3.3};
-
-
-for (int i = 0; i < names.Length; i++)
+﻿class Program
 {
-    Console.WriteLine(names[i]);
-}
+    static void Main()
+    {
+        string[] names = { "Anderson", "Silva", "João" };
+        
+    }
 
-for (int i = 0; i < numbers.Length; i++)
-{
-    Console.WriteLine(numbers[i]);
-}
-
-for (int i = 0; i < floats.Length; i++)
-{
-    Console.WriteLine(floats[i]);
 }
