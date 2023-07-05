cf77a749db0a84b9d59a01c65cce9c40a0e8dedd
Anderson <eusouanderson@outlook.com>
2023-07-05 17:19:01 -0300

    "update"

--- Program.cs
+++ Program.cs
@@ -13,3 +13,16 @@ int year = int.Parse(Console.ReadLine());
 int age = 2023 - year;
 
 Console.WriteLine($"Você nasceu no ano: {year} e seu nome é: {name} {lastname} e sua idade é: {age}");
+
+if (age >= 18 && age <= 60)
+{
+    Console.WriteLine("Você é maior de idade e voto é obrigatório");
+}
+else if (age < 18 && age > 15)
+{
+    Console.WriteLine("Você é menor de idade e seu voto não é obrigatório");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade e você ainda não vota ");
+}
